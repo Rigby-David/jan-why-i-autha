d26a7d73407b64dbd632709df1038ae7b0756231
KASHI TAMANG <tamang.kashi@gmail.com>
2023-01-24 14:14:07 -0800

    adding working logout button

--- other-page/other-page.js
+++ other-page/other-page.js
@@ -1,6 +1,12 @@
 // use checkAuth function to redirect is user not authenticated
 
-import { checkAuth } from '../fetch-utils.js';
+import { checkAuth, logout } from '../fetch-utils.js';
+
+const logoutBtn = document.getElementById('logout');
 
 // add event listener to the logout button and call logout
+logoutBtn.addEventListener('click', async () => {
+    await logout();
+});
+
 checkAuth();
